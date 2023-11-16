852c1fe6e58513196c36a0ab318791a7a4803962
thibaultmougin <102171357+thibaultmougin@users.noreply.github.com>
2023-11-17 00:35:13 +0100

    job file

---
+++ job_mpi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=MyJob
+#SBATCH --output=output_%j.txt
+#SBATCH --error=error_%j.txt
+
+#SBATCH --partition=cpu_test
+#SBATCH --account=ams301
+
+#SBATCH --ntasks=4
+#SBATCH --time=00:10:00
+
+## load modules
+module load cmake
+module load gcc
+module load gmsh
+module load openmpi
+
+## execution
+mpirun -display-map ${SLURM_SUBMIT_DIR}/solver
