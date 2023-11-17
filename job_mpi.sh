d02a1b6b99f1170540719a3d451be863be13f9ce
thibaultmougin <thibault.mougin01@gmail.com>
2023-11-17 10:48:29 +0100

    sbatch file

--- job_mpi.sh
+++ job_mpi.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-for i in 1 2 4 8 16 32 64
-do
+
 #SBATCH --job-name=MyJob
 #SBATCH --output=output_%j.txt
 #SBATCH --error=error_%j.txt
@@ -8,7 +7,7 @@ do
 #SBATCH --partition=cpu_test
 #SBATCH --account=ams301
 
-#SBATCH --ntasks=i
+#SBATCH --ntasks=1
 #SBATCH --time=00:10:00
 
 ## load modules
@@ -19,4 +18,4 @@ module load openmpi
 
 ## execution
 mpirun -display-map ${SLURM_SUBMIT_DIR}/solver
-done
+
